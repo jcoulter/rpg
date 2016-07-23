ee50650c17b2fe58c7174de145aaedd07584d367
Jacob Coulter <jacob.coulter@leandog.com>
2016-07-23 14:35:45 -0400

    Added level to Character

--- lib/character.rb
+++ lib/character.rb
@@ -1,7 +1,9 @@
 class Character
 attr_reader :health
+attr_reader :level
 
   def initialize
     @health = 1000
+    @level = 1
   end
 end
--- specs/character_spec.rb
+++ specs/character_spec.rb
@@ -6,4 +6,9 @@ describe 'Character' do
   it 'should initialize health' do
     expect(Character.new.health).to eq(1000)
   end
+
+  it 'should initialize level' do
+    expect(Character.new.level).to eq(1)
+  end
+
 end
